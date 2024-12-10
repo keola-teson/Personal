54871f62e51f057500c2fd203cd9d047ea8787b0
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-12-09 20:32:30 -1000

    Create Main.java

---
+++ Sorting/Main.java
@@ -0,0 +1,10 @@
+package sorting;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Sorting insertionSort = new Sorting("1 12 4 11 20 2");
+		insertionSort.addIntToArraySpace();
+	}
+}
